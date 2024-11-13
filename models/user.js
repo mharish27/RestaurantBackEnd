bdf73cddae1aa808c81db674ca618820836a33c9
Harish Kumar Manepalli <114908558+mharish27@users.noreply.github.com>
2024-11-13 14:47:56 -0800

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -1,1 +1,30 @@
+const mongoose = require('mongoose')
 
+const userSchema = new mongoose.Schema({
+    first_name: {
+      type: String,
+      required: true
+    },
+    last_name: {
+        type: String,
+        required: true
+    },
+    token: {
+        type: String
+    },
+    email_id: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+      type: Number,
+      required: true
+    },
+    creation_date: {
+      type: Date,
+      required: true,
+      default: Date.now
+    }
+  })
+  
+  module.exports = mongoose.model('user', userSchema)
