0b1ce76b5ada1e4621d7c14662998e41331af6bc
Harish Kumar Manepalli <114908558+mharish27@users.noreply.github.com>
2024-11-13 14:48:27 -0800

    Add files via upload

--- routes/register.js
+++ routes/register.js
@@ -1,1 +1,29 @@
+const express = require('express')
+const User = require('../models/user')
+const router = express.Router()
 
+router.get('/', async (req, res) => {
+    try {
+      const users = await User.find()
+      res.json(users)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  })
+
+router.post('/', async (req, res) => {
+    const user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        phone_number: req.body.phone_number,
+        email_id: req.body.email_id
+    })
+    try {
+      const newUser= await user.save()
+      res.status(201).json(newUser)
+    } catch (err) {
+      res.status(400).json({ message: err.message })
+    }
+  })
+
+module.exports = router
